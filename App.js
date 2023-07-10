2b85f9d2d08df5c8438f0c2b989e9e132334c5fe
artyomakulov <artyomakulov@gmail.com>
2023-07-10 16:45:31 +0300

    add screens with fake content

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import "react-native-gesture-handler";
-import RegistrationScreen from "./Screens/RegistrationScreen";
-import LoginScreen from "./Screens/LoginScreen";
+
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import RegistrationScreen from "./Screens/RegistrationScreen";
+import LoginScreen from "./Screens/LoginScreen";
 
 const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator()
 
 export default function App() {
   return (
     <NavigationContainer>
-      <AuthStack.Navigator>
+      <AuthStack.Navigator initialRouteName="Registration">
         <AuthStack.Screen
           name="Registration"
           component={RegistrationScreen}
